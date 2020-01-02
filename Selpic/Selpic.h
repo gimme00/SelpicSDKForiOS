c6b13c0665821bbf7973aaba178fd9b755f73786
xudihua <13076997932@163.com>
2020-01-02 18:34:24 +0800

    Initial Commit

---
+++ Selpic/Selpic.h
@@ -0,0 +1,19 @@
+//
+//  Selpic.h
+//  Selpic
+//
+//  Created by 徐迪华 on 2020/1/2.
+//  Copyright © 2020 xdh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Selpic.
+FOUNDATION_EXPORT double SelpicVersionNumber;
+
+//! Project version string for Selpic.
+FOUNDATION_EXPORT const unsigned char SelpicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Selpic/PublicHeader.h>
+
+
